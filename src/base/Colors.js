4529892a14652a1ba860a4512fab45ed37472268
kriptonhaz <kriptonhaz@gmail.com>
2020-11-21 19:13:48 +0700

    add color

--- src/base/Colors.js
+++ src/base/Colors.js
@@ -1,12 +1,15 @@
 const Colors = {
   white: '#FFFFFF',
   black: '#000000',
+  blackText: '#2A2A2A',
   yellowPrimary: '#F5AF19',
   grayText: '#686868',
   grayText2: '#7B7B7B',
+  grayText3: '#707070',
   grayOutline: '#00000063',
   grayOutlineCircle: '#707070',
   bluePrimary: '#089AEB',
+  blueText: '#344FD9',
   greenPrimary: '#109DA2'
 }
 
