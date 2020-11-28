4a5ba9e11f306693f6f95688a081c0774f201106
kriptonhaz <kriptonhaz@gmail.com>
2020-11-28 11:19:18 +0700

    fix card promo

--- src/components/Card/CardPromo.js
+++ src/components/Card/CardPromo.js
@@ -1,17 +1,41 @@
 import React from 'react';
-import { TouchableOpacity, Image, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native';
 import { heightPercentage, widthPercentage } from '../../helper/dimension';
+import { Fonts, Colors, Dimens } from '../../base';
 
-export default function CardPromo(props){
-  return(
-    <TouchableOpacity style={styles.rootContainer} onPress={props.onPress}>
+export default function CardPromo({title, image, onPress}){
+
+  const ImagePromo = () => {
+    return(
       <Image 
-        source={{ uri: props.image }}
+        source={{ uri: image }}
         style={styles.image}
         resizeMode={'cover'}
       />
-    </TouchableOpacity>
-  )
+    )
+  }
+
+  if(title){
+    return(
+      <View style={[styles.rootContainer, { aspectRatio: 329/130, marginTop: 0, marginBottom: 0}]}>
+        <View style={styles.headerWrapper}>
+          <Text style={styles.textTitle}>{title}</Text>
+          <TouchableOpacity onPress={onPress}>
+            <Text style={styles.textDetail}>Lihat Selengkapnya</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={{ width: '100%', height: '90%'}}>
+          <ImagePromo />
+        </View>
+      </View>
+    )
+  } else {
+    return(
+      <TouchableOpacity style={styles.rootContainer} onPress={onPress}>
+        <ImagePromo />
+      </TouchableOpacity>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
@@ -24,8 +48,26 @@ const styles = StyleSheet.create({
     marginTop: heightPercentage(1),
     marginBottom: heightPercentage(1)
   },
+  headerWrapper: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 5
+  },
+  textTitle: {
+    fontFamily: Fonts.poppinsSemiBold,
+    fontSize: Dimens.FONT_SIZE_12,
+    color: Colors.blackTextPackage
+  },
+  textDetail: {
+    fontFamily: Fonts.poppinsRegular,
+    fontSize: Dimens.FONT_SIZE_9,
+    color: Colors.bluePrimary
+  },
   image: {
     width: '100%',
-    height: '100%'
+    height: undefined,
+    aspectRatio: 329/100
   }
 })
